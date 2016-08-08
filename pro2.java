ad77801ee00da1ee9f034dfabb02d998d760b069
Nandhini11 <nandhinias11@gmail.com>
2016-08-07 22:50:09 -0700

    Add files via upload

---
+++ pro2.java
@@ -0,0 +1,22 @@
+
+package smapless;
+
+public class strindcon {
+    public static void main(String[] args){
+        String s1="deep";
+        String s2="Nandhini";
+        int a=s1.length();
+        int b=s2.length();
+        int k=2;
+        if(a>b){
+            
+        }else{
+            String s3=s2.substring(b-2,b);
+            char s4=s2.charAt(1);
+            char s5=s2.charAt(6);
+            System.out.println(""+(s3+s1+s4+s5));
+            
+        }
+    }
+    
+}
