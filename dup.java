799376421dda4fd1f72daad166a8b3a4b1173ee3
Nandhini11 <nandhinias11@gmail.com>
2016-08-07 22:16:41 -0700

    Add files via upload

---
+++ dup.java
@@ -0,0 +1,27 @@
+
+package stringdup;
+
+import java.util.Scanner;
+
+pubic class Stringdup {
+
+ 
+    public static void main(String[] args) {
+        // TODO code application logic here
+      
+        System.out.println("enter the no");
+        Scanner in=new Scanner(System.in);
+        int n= in.nextInt();
+         if((100-n)<=10)
+             {
+             System.out.println("true");
+        if((200-n)<=10){
+             System.out.println("true");
+        }else
+                     System.out.println("sakdsajhd");
+            }else
+             System.out.println(" ........");
+    }
+    }
+    
+
