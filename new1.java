376e884edaff3871ddd9cda307092587a4e7bc99
Nandhini11 <nandhinias11@gmail.com>
2016-08-07 23:29:49 -0700

    Add files via upload

---
+++ new1.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fripro;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Nandhini
+ */
+public class activity {
+    public static void main(String[] args){
+        System.out.println("enter the number sum and difference");
+        Scanner sc=new Scanner(System.in);
+        int a,b;
+        int sum=sc.nextInt();
+        int dif=sc.nextInt();
+        int add=sum+dif;
+        a=add/2;
+        b=sum-a;
+        System.out.println("a value is "+a);
+        System.out.println("b value is  "+b);
+       
+       }
+        
+    }
+        
+        
+    
+        
+    
+
