5a41b2f49c0d7a3e31f8feec1c066286f63abfc0
Nandhini11 <nandhinias11@gmail.com>
2016-08-07 23:14:30 -0700

    Add files via upload

---
+++ pro5.java
@@ -0,0 +1,32 @@
+
+package smapless;
+
+import java.util.Scanner;
+public class activity5 {
+    public static void main(String[]args){
+        System.out.println("enter the two no");
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println("enter the operation to be performed");
+        Scanner sc1=new Scanner(System.in);
+        String c=sc1.nextLine();
+        String d="+",e="-",f="*", g="/";
+        if(c.equals(d)){
+            int add=a+b;
+            System.out.println("addition of two no is  "+add);
+        }else if(c.equals(e)){
+            int sub=a-b;
+            System.out.println("sum is "+sub);
+            
+        }else if(c.equals(f)){
+            int mul=a*b;
+            System.out.println("mul is  "+mul);
+            
+        }else if(c.equals(g)){
+            int div=a/b;
+            System.out.println("div is"+div);
+        }
+    }
+    
+}
