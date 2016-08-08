67012ad09b83d2c64cb0a5835c25120e3ce27615
Nandhini11 <nandhinias11@gmail.com>
2016-08-07 22:06:38 -0700

    Add files via upload

---
+++ string2.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stringdup;
+
+import java.util.Scanner;
+
+public class Stringdup {
+
+    
+    public static void main(String[] args) {
+       
+        System.out.println("enter the no");
+        Scanner in=new Scanner(System.in);
+        int n= in.nextInt();
+         if((100-n)<=10)
+             {
+             System.out.println("true");
+        if((200-n)<=10){
+             System.out.println("true");
+        }else
+                     System.out.println("sakdsajhd");
+            }else
+             System.out.println(" ........");
+    }
+    }
+    
+
